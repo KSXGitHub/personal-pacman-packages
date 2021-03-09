ef9051ec29b4949b66b33664da5f36f0c0cfadab
khai96_ <hvksmr1996@gmail.com>
2021-03-09 11:33:25 +0700

    Exclude empty lines

--- members/dino-runner-miniserve/start-dino-runner-server.bash
+++ members/dino-runner-miniserve/start-dino-runner-server.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 set -o errexit -o pipefail -o nounset
-mapfile -t flags < <(grep --invert-match '^#' "$DINO_RUNNER_SERVER_FLAGS_FILE")
+mapfile -t flags < <(grep -E --invert-match '^#|^\s*$' "$DINO_RUNNER_SERVER_FLAGS_FILE")
 exec miniserve /usr/lib/dino-runner-data/ "${flags[@]}"
